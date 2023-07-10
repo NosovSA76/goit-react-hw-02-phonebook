42263847664731b14f8acb5378fb2305728fd27d
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-07-10 10:37:58 +0300

    Delete

--- src/components/countMessage/CountMessage.styled.jsx
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-export const CountMessageStyled = styled.p`
-text-align: center;
-margin: ${props => props.theme.space[4]}px;
-font-weight: ${props => props.theme.fontWeights.semiBold};
-`
